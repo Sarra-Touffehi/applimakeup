aac2192f384fe613173106c244d4bb523f2f33ef
sarra touffehi <sarratouffehi70@gmail.com>
2022-11-21 23:18:02 +0100

    version 4

---
+++ src/app/components/headersarra/headersarra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headersarra',
+  templateUrl: './headersarra.component.html',
+  styleUrls: ['./headersarra.component.css']
+})
+export class HeadersarraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
